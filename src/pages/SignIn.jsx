75606a6aa74918411607ce6e71ad5909d46e4153
BlaskVN <124863795+BlaskVN@users.noreply.github.com>
2024-05-30 14:54:01 +0700

    Login Page add

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,25 +1,28 @@
 import { FaUser } from "react-icons/fa6";
 import { FaLock } from "react-icons/fa";
-    const loginForm=()=>{
-    return (
-        <div className="wrapper">
-            <h1>Sign In</h1>
-            <form action=""> 
-                <div className="input-field">  
-                    <input type="text" placeholder="Username" required/>
-                <FaUser />
-                <div/>
-                <div className="input-field">  // Add a closing tag here
+import React from "react";
+import '../Signin.css'
+    const SignIn = () => {
+        return (
+            <div className="wrapper">
+                <form action="">
+                    <h1>Đăng Nhập</h1>
+                    </form>
+                    <div className="inputbox ">
+                        <input type="text" placeholder="Username"required/>
+                        <FaUser className="icon"/>
+                    </div>
+                    <div className="inputbox ">
+                        <input type="password" placeholder="Password"required/>
+                        <FaLock className="icon"/>
+                    </div>
+                    <button type="submit">Sign In</button>
+                    <div className="register">
+                        <p>Don't have an account ? <a href="#"> SignUp </a> </p>
+                    </div>
 
-                    <input type="password" name="password" />
-            
-                <FaLock />
-                <br />
-                <button type="submit">Sign In</button>
-                
-            </form>
-        </div>
-        
-    );
-}
+            </div>
+
+        );
+    };
 export default SignIn;
