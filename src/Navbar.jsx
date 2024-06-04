20858198990b74b9232b7f6b0cba2ce1a678b4e7
BlaskVN <124863795+BlaskVN@users.noreply.github.com>
2024-06-04 07:50:45 +0700

    .

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -19,9 +19,9 @@ function Navbar() {
         <a href="/" className="site-title">LGTV</a>
         <ul>
           <li className="active"><a href="/list">Danh sách phim</a></li>
-          <li className="active"><a href="123">Lịch chiếu rạp</a></li>
-          <li className="active"><a href="#">Phim mới</a></li>
-          <li className="active"><a href="#">Sắp ra mắt</a></li>
+          <li className="active"><a href="/lichchieu">Lịch chiếu rạp</a></li>
+          <li className="active"><a href="/newfilm">Phim mới</a></li>
+          <li className="active"><a href="/incoming">Sắp ra mắt</a></li>
           <li className="active"><a href="/about">Liên hệ</a></li>
         </ul>
         <ul className="nav-right">
